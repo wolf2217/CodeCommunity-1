0b18389d20112fc800dd76e8f7cbd419d206f46c
Jason Caswell <jason.caswell@hotmail.com>
2018-11-17 19:56:01 -0700

    Database added with login

--- CodeCommunity/Form1.Designer.cs
+++ CodeCommunity/Form1.Designer.cs
@@ -28,13 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtLoginPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLoginUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlRegister = new System.Windows.Forms.Panel();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNewUser = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnlLogin.SuspendLayout();
+            this.pnlRegister.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.pnlLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.txtLoginPass);
+            this.pnlLogin.Controls.Add(this.label2);
+            this.pnlLogin.Controls.Add(this.txtLoginUser);
+            this.pnlLogin.Controls.Add(this.label1);
+            this.pnlLogin.Location = new System.Drawing.Point(12, 12);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(305, 242);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(37)))), ((int)(((byte)(45)))));
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(199, 187);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 41);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtLoginPass
+            // 
+            this.txtLoginPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtLoginPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLoginPass.ForeColor = System.Drawing.Color.Silver;
+            this.txtLoginPass.Location = new System.Drawing.Point(36, 152);
+            this.txtLoginPass.Name = "txtLoginPass";
+            this.txtLoginPass.Size = new System.Drawing.Size(238, 29);
+            this.txtLoginPass.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtLoginUser
+            // 
+            this.txtLoginUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtLoginUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLoginUser.ForeColor = System.Drawing.Color.Silver;
+            this.txtLoginUser.Location = new System.Drawing.Point(36, 75);
+            this.txtLoginUser.Name = "txtLoginUser";
+            this.txtLoginUser.Size = new System.Drawing.Size(238, 29);
+            this.txtLoginUser.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // pnlRegister
+            // 
+            this.pnlRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.pnlRegister.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlRegister.Controls.Add(this.btnSignup);
+            this.pnlRegister.Controls.Add(this.txtNewPass);
+            this.pnlRegister.Controls.Add(this.label3);
+            this.pnlRegister.Controls.Add(this.txtNewUser);
+            this.pnlRegister.Controls.Add(this.label4);
+            this.pnlRegister.Location = new System.Drawing.Point(342, 12);
+            this.pnlRegister.Name = "pnlRegister";
+            this.pnlRegister.Size = new System.Drawing.Size(305, 242);
+            this.pnlRegister.TabIndex = 1;
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(37)))), ((int)(((byte)(45)))));
+            this.btnSignup.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnSignup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignup.Location = new System.Drawing.Point(169, 187);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(104, 41);
+            this.btnSignup.TabIndex = 9;
+            this.btnSignup.Text = "Sign Up";
+            this.btnSignup.UseVisualStyleBackColor = false;
+            this.btnSignup.Click += new System.EventHandler(this.btnSignup_Click);
+            // 
+            // txtNewPass
+            // 
+            this.txtNewPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtNewPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNewPass.ForeColor = System.Drawing.Color.Silver;
+            this.txtNewPass.Location = new System.Drawing.Point(35, 152);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.Size = new System.Drawing.Size(238, 29);
+            this.txtNewPass.TabIndex = 8;
+            this.txtNewPass.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password";
+            // 
+            // txtNewUser
+            // 
+            this.txtNewUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtNewUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNewUser.ForeColor = System.Drawing.Color.Silver;
+            this.txtNewUser.Location = new System.Drawing.Point(35, 75);
+            this.txtNewUser.Name = "txtNewUser";
+            this.txtNewUser.Size = new System.Drawing.Size(238, 29);
+            this.txtNewUser.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(663, 267);
+            this.Controls.Add(this.pnlRegister);
+            this.Controls.Add(this.pnlLogin);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login/Register";
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.pnlRegister.ResumeLayout(false);
+            this.pnlRegister.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtLoginPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLoginUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlRegister;
+        private System.Windows.Forms.Button btnSignup;
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNewUser;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- CodeCommunity/Form1.cs
+++ CodeCommunity/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,68 @@ namespace CodeCommunity
 {
     public partial class Form1 : Form
     {
+        Linq.dbStructureDataContext db = new Linq.dbStructureDataContext();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+            var checkUser = from cc_users in db.cc_users
+                            where cc_users.UserName == txtNewUser.Text
+                            select cc_users;
+
+            foreach (var user in checkUser)
+            {
+                if(user.UserName == txtNewUser.Text)
+                {
+                    MessageBox.Show("Username already exists");
+                }
+                else
+                {
+
+
+                    
+
+                    var createUser = new Linq.cc_user()
+                    {
+                        UserName = txtNewUser.Text,
+                        Password = txtNewPass.Text,
+                        Created = DateTime.Now.ToLongDateString()
+                    };
+                    db.cc_users.InsertOnSubmit(createUser);
+                    try
+                    {
+                        db.SubmitChanges();
+                        MessageBox.Show("User created");
+                        Properties.Settings.Default.Username = txtNewUser.Text;
+                        Properties.Settings.Default.Save();
+                    }catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var checkLogin = from cc_users in db.cc_users
+                             where cc_users.UserName == txtLoginUser.Text && cc_users.Password == txtLoginPass.Text
+                             select cc_users;
+            foreach(var user in checkLogin)
+            {
+                if(txtLoginUser.Text == user.UserName && txtLoginPass.Text == user.Password)
+                {
+                    Dashboard dashboard = new Dashboard();
+                    dashboard.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login failed");
+                }
+            }
         }
     }
 }
