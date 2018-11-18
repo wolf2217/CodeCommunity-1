0d32c95f6ea5b6927cf6f0f9cfb2ecde1c334059
Jason Caswell <jason.caswell@hotmail.com>
2018-11-18 00:29:21 -0700

    Users class created

--- CodeCommunity/Form1.cs
+++ CodeCommunity/Form1.cs
@@ -25,76 +25,13 @@ namespace CodeCommunity
 
         private void btnSignup_Click(object sender, EventArgs e)
         {
-            //create salt
-            byte[] salt;
-            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
-            //generate hash with salt
-            string hash = PasswordHashHelper.Hash(txtNewPass.Text, salt, 1000);
-
-            //MessageBox.Show(hash + "\r\n" + Convert.ToBase64String(salt));
-
-            var checkUser = from cc_users in db.cc_users
-                            where cc_users.UserName == txtNewUser.Text
-                            select cc_users.UserName;
-                           
-
-            if(checkUser.Count() == 1)
-            {
-                MessageBox.Show("Username already exists");
-            }
-            else
-            {
-                var createUser = new Linq.cc_user()
-                {
-                    UserName = txtNewUser.Text,
-                    Password = hash,
-                    Created = DateTime.Now.ToLongDateString(),
-                    Salt = Convert.ToBase64String(salt)
-                    
-                };
-                db.cc_users.InsertOnSubmit(createUser);
-                try
-                {
-                    db.SubmitChanges();
-                    MessageBox.Show("User created");
-                    Properties.Settings.Default.Username = txtNewUser.Text;
-                    Properties.Settings.Default.Save();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
+            Model.Users.CreateUser();
         }
 
-        //I changed the select new here also...now it selects the cc_users username and password
+        
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            var checkLogin = from cc_users in db.cc_users
-                             where cc_users.UserName == txtLoginUser.Text
-                             select new
-                             {
-                                 username = cc_users.UserName,
-                                 password = cc_users.Password, 
-                                 salt = cc_users.Salt
-                             };
-
-            foreach(var user in checkLogin)
-            {
-                //var saltByte = new byte[user.salt.Length];
-                if (PasswordHashHelper.Verify(txtLoginPass.Text, user.password))
-                {
-                    //MessageBox.Show("Passwords Match");
-                    Properties.Settings.Default.Username = txtLoginUser.Text;
-                    Properties.Settings.Default.Save();
-                    Dashboard dashboard = new Dashboard();
-                    dashboard.Show();
-                }
-                else
-                {
-                    MessageBox.Show("Login failed");
-                }
-            }
+            Model.Users.Login();
         }
     }
 }
